4f3ff46c7e866281c3d7764d5cb54aad08e168aa
Sener DEMIRAL <sener.demiral@outlook.com>
2024-06-07 01:48:18 +0300

    07.06.24

--- Hadisene/Lib/OO.cs
+++ Hadisene/Lib/OO.cs
@@ -45,8 +45,17 @@ public sealed class OO
 	public int MnVc;	// Msg Not Viewed Count (New) for this Usr
 
 	public string IoC = "I";	// Footer Input or Confirm
-	public string SoF = "S";	// Footer Start or Finish
+	public string SoF = "S";    // Footer Start or Finish
 
+	public bool InfRO = true;
+	public bool ReqRO = true;
+	public bool SbjRO = true;
+	public bool TskRO = true;
+	public bool ActRO = true;
+	public bool RSDro = true;
+	public bool RFDro = true;
+	public bool ASDro = true;
+	public bool AFDro = true;
 
 	public string? RSDf => RSD?.ToString("dd.MM.yy HH:mm");
 	public string? RFDf => RFD?.ToString("dd.MM.yy HH:mm");
@@ -54,4 +63,9 @@ public sealed class OO
 	public string? AFDf => AFD?.ToString("dd.MM.yy HH:mm");
 
 	public string MnVcF => MnVc > 99 ? "+99" : MnVc.ToString("#");
+
+	public OO ShallowCopy()
+	{
+		return (OO)this.MemberwiseClone();
+	}
 }
