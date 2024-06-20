b895fbecc7a1bc4bdcebb9bb8e1f273c57ef90cb
Sener DEMIRAL <sener.demiral@outlook.com>
2024-06-20 23:37:02 +0300

    20.06.24

---
+++ Hadisene/Lib/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Hadisene.Lib;
+
+public static class Extensions
+{
+	public static string? ToS(this DateTime? input)
+	{
+		if (input != null)
+		{
+			return input?.TimeOfDay != TimeSpan.Zero
+				? input?.ToString("dd.MM.yy hh:mm")
+				: input?.ToString("dd.MM.yy");
+		}
+		return "⚠️";
+	}
+}
