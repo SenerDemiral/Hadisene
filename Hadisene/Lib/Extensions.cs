cf684877a76367b0ccf1b04518014dfca6779ad9
Sener DEMIRAL <sener.demiral@outlook.com>
2024-06-21 13:48:04 +0300

    21.06.24

--- Hadisene/Lib/Extensions.cs
+++ Hadisene/Lib/Extensions.cs
@@ -4,12 +4,36 @@ public static class Extensions
 {
 	public static string? ToS(this DateTime? input)
 	{
-		if (input != null)
+		if (!input.HasValue)
+			return "⚠️";
+
+		DateTime dt = input.Value;
+		string result;
+
+		if (DateTime.Today == dt.Date)
+			result = "Bugün";
+		else if (DateTime.Today.AddDays(-1) == dt.Date)
+			result = "Dün";
+		else if (DateTime.Today.AddDays(1) == dt.Date)
+			result = "Yarın";
+		else
 		{
-			return input?.TimeOfDay != TimeSpan.Zero
-				? input?.ToString("dd.MM.yy hh:mm")
-				: input?.ToString("dd.MM.yy");
+			if (dt.Year == DateTime.Today.Year)
+				result = dt.ToString("dd-MMM");
+			else
+				result = dt.ToString("dd.MM.yy");
 		}
-		return "⚠️";
+		if (dt.TimeOfDay != TimeSpan.Zero)
+		{
+			result = result + " " + dt.ToString("HH:mm");
+		}
+
+		return result;
+
+		//if (DateTime.Today == dt.Date)
+		//	result = "Bugün" + (hasTime ? dt.TimeOfDay.ToString("hh:mm") : "");
+		//return input?.TimeOfDay != TimeSpan.Zero
+		//	? input?.ToString("dd.MM.yy hh:mm")
+		//	: input?.ToString("dd.MM.yy");
 	}
 }
